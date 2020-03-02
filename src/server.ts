3f3880b2c3dc5753cd67024040b900fff7b9254d
Steven <steven.msmgroup@gmail.com>
2020-03-03 01:46:16 +0700

    Add RESTful endpoint to filter image

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -31,6 +31,26 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   //! END @TODO1
   
+  app.get( "/filteredimage/", async ( req: Request, res: Response, next) => {
+    let { image_url } = req.query;
+
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`Image URL is required`);
+    }
+
+    let filteredImage = await filterImageFromURL(image_url);
+
+    res.status(200)
+        .sendFile(filteredImage, function (err) {
+          if (err) {
+            next(err)
+          } else {
+            deleteLocalFiles([filteredImage])
+          }
+        } );
+  } );
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
